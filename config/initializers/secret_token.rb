6f136f991d4290570e2405f17173b55508c5efb6
andylinteau <andy.linteau@buystand.com>
2013-06-03 00:39:11 +0800

    create 8065Deals App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Deals8065::Application.config.secret_token = '30c331bd18641ee4fb83854d45ea4e893bfab1225cad3b90b4f34a7919532b25e600c2a6411d488e82b0f46dff9b9196bc7d1bd39fcbb3080960aa32fdcce7b6'
