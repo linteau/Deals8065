5c5c93862dfd1cedaeccb8cb65b383f58cf58954
andylinteau <andy.linteau@buystand.com>
2013-06-04 06:35:38 +0800

    add name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130602183107) do
+ActiveRecord::Schema.define(:version => 20130602191740) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "",    :null => false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20130602183107) do
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
     t.boolean  "admin",                  :default => false
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
